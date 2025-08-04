7c54e36ced1c23846a22f84fedc1646463d30d42
Kristian <10348902@pm.me>
2025-08-04 20:50:00 +0200

    feat: observability setup

---
+++ Conduit.API/Extensions/ObservabilityExtensions.cs
@@ -0,0 +1,30 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Conduit.API.Extensions;
+
+public static class ObservabilityExtensions
+{
+    public static void AddObservability(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOpenTelemetry()
+            .ConfigureResource(r => r
+                .AddService(
+                    serviceName: "cypherly.identity.svc",
+                    serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString(),
+                    serviceInstanceId: Environment.MachineName))
+
+            .WithTracing(b => b
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddRedisInstrumentation()
+                .AddOtlpExporter())
+            
+            .WithMetrics(b => b
+                .AddRuntimeInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddPrometheusExporter());
+    }
+}
--- Conduit.API/Program.cs
+++ Conduit.API/Program.cs
@@ -1,4 +1,39 @@
+using System.Reflection;
+using Conduit.API.Extensions;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region Appsettings
+
+var env = builder.Environment;
+
+var configuration = builder.Configuration;
+configuration
+    .AddJsonFile("appsettings.json", false, true)
+    .AddEnvironmentVariables();
+
+if (env.IsDevelopment())
+{
+    configuration.AddJsonFile($"appsettings.{Environments.Development}.json", true, true);
+    configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+}
+
+#endregion
+
+#region Logger
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddObservability(configuration);
+
+Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+#endregion
 
 builder.Services.AddOpenApi();
 
@@ -11,4 +46,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
+app.MapPrometheusScrapingEndpoint();
+app.UseSerilogRequestLogging();
+
+try
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+// Required for integration tests
+public partial class Program { }
