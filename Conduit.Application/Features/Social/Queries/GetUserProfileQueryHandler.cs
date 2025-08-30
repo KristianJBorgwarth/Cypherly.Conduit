9608e21b088e631c635829512f106759fb2049c9
Kristian <10348902@pm.me>
2025-08-30 18:29:53 +0200

    feat: added con id filter

--- Conduit.Application/Features/Social/Queries/GetUserProfileQueryHandler.cs
+++ Conduit.Application/Features/Social/Queries/GetUserProfileQueryHandler.cs
@@ -8,18 +8,21 @@ namespace Conduit.Application.Features.Social.Queries;
 public sealed class GetUserProfileQueryHandler(
     IUserProfileProvider userProfileProvider,
     IConnectionIdProvider connectionIdProvider,
-    ILogger<GetUserProfileQueryHandler> logger) 
+    ILogger<GetUserProfileQueryHandler> logger)
     : IQueryHandler<GetUserProfileQuery, GetUserProfileDto>
 {
-    public async Task<Result<GetUserProfileDto>> Handle(GetUserProfileQuery request, CancellationToken cancellationToken)
+    public async Task<Result<GetUserProfileDto>> Handle(GetUserProfileQuery request,
+        CancellationToken cancellationToken)
     {
         try
         {
             var userProfile = await userProfileProvider.GetUserProfile(request.ExclusiveConnectionId, cancellationToken);
-            if (userProfile is null) 
+            if (userProfile is null)
                 return Result.Fail<GetUserProfileDto>(Error.NotFound("UserProfile not found"));
-            
+
             var connectionIds = await connectionIdProvider.GetConnectionIds(cancellationToken);
+
+            connectionIds = RemoveConnectionId(connectionIds, request.ExclusiveConnectionId);
             
             var dto = new GetUserProfileDto(userProfile, connectionIds);
             return Result.Ok(dto);
@@ -30,4 +33,15 @@ public sealed class GetUserProfileQueryHandler(
             return Result.Fail<GetUserProfileDto>(Error.NotFound("UserProfile not found"));
         }
     }
+
+    /// <summary>
+    /// Exclude the current connection id from the list of connection ids
+    /// to avoid sending the id of the requesting client back to itself.
+    /// </summary>
+    /// <param name="connectionIds">ids to modify</param>
+    /// <param name="exclusiveConnectionId">the ids to filter out</param>
+    /// <returns></returns>
+    private static List<Guid> RemoveConnectionId(IReadOnlyCollection<Guid> connectionIds,
+        Guid exclusiveConnectionId) =>
+        connectionIds.Where(id => id != exclusiveConnectionId).ToList();
 }
