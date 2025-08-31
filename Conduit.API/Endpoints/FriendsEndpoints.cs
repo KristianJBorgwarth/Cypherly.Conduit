97527f8e2f17874287a8921ae26b8b1fcacc18df
Kristian <10348902@pm.me>
2025-08-31 12:42:38 +0200

    feat: delete friendship endpoint (#10)

--- Conduit.API/Endpoints/FriendsEndpoints.cs
+++ Conduit.API/Endpoints/FriendsEndpoints.cs
@@ -1,6 +1,7 @@
 using Conduit.API.Common;
 using Conduit.API.Requests;
 using Conduit.Application.Features.Friends.Commands.Create;
+using Conduit.Application.Features.Friends.Commands.Delete;
 using Conduit.Application.Features.Friends.Queries.GetFriendRequests;
 using Conduit.Application.Features.Friends.Queries.GetFriends;
 using MediatR;
@@ -42,6 +43,14 @@ public sealed class FriendsEndpoints : IEndpoint
         })
         .Produces<IReadOnlyCollection<GetFriendRequestsDto>>()
         .Produces(StatusCodes.Status204NoContent)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+        
+        group.MapDelete("", async ([FromServices] ISender sender, [FromQuery] string tag) =>
+        {
+            var result = await sender.Send(new DeleteFriendshipCommand { FriendTag = tag });
+            return result.Success ? Results.Ok() : result.ToProblemDetails();
+        })
+        .Produces(StatusCodes.Status200OK)
         .ProducesProblem(StatusCodes.Status400BadRequest);
     }
 }
